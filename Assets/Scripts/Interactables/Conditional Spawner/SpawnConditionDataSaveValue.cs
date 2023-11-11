7baadb796ac9792fdff466772f6683f9177413ad
FFerreiraLaValle <fferreiralavalle@gmail.com>
2023-11-11 21:57:46 +0100

    Refactor: Implemented 0x44 changes for SpawnConditionDataSaveValue. More to go.

--- Assets/Scripts/Interactables/Conditional Spawner/SpawnConditionDataSaveValue.cs
+++ Assets/Scripts/Interactables/Conditional Spawner/SpawnConditionDataSaveValue.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 public class SpawnConditionDataSaveValue
 {
     public SaveElementId saveVariable;
+    public string saveElementId;
+    public SaveElementNumber saveElement;
     public SpawnConditionDataValueCheck condition;
     public float value;
 
+    public string GetId()
+    {
+        if (string.IsNullOrEmpty(saveElementId))
+        {
+            if (saveElement != null)
+            {
+                // TODO: This is legacy and should be removed later
+                return saveVariable.ToString();
+            }
+            return saveElement.GetId();
+        }
+        return saveElementId;
+    }
+
     public bool IsTrue()
     {
-        float saveValue = SaveMaster.Instance.GetSaveElementFloat(saveVariable.ToString());
+        float saveValue = SaveMaster.Instance.GetSaveElementFloat(GetId());
         switch (condition)
         {
             case SpawnConditionDataValueCheck.IsLessThan:
